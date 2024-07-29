2f93c59f51d183faea7d7c2291174914ab5b8546
Velten Stoyann <krysto.dev@gmail.com>
2024-07-29 18:51:21 +1100

    initial commit and theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        text: '#0a010e',
+        background: '#fbf5fe',
+        primary: '#bb2bec',
+        secondary: '#c1f488',
+        accent: '#51f088',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'linear-primary-secondary': 'linear-gradient(#bb2bec, #c1f488)',
+        'linear-primary-accent': 'linear-gradient(#bb2bec, #51f088)',
+        'linear-secondary-accent': 'linear-gradient(#c1f488, #51f088)',
+        'radial-primary-secondary': 'radial-gradient(#bb2bec, #c1f488)',
+        'radial-primary-accent': 'radial-gradient(#bb2bec, #51f088)',
+        'radial-secondary-accent': 'radial-gradient(#c1f488, #51f088)',
       },
     },
   },
   plugins: [],
-};
+}
